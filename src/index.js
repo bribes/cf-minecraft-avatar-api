de821adc7ed7e9e8e1dfc4978b4e67cc458a8a90
Faav <faavemc@gmail.com>
2024-08-31 00:14:16 -0400

    Add more customizations

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { Hono } from 'hono'
-import { PhotonImage, SamplingFilter, crop, resize, blend } from "@cf-wasm/photon";
+import { PhotonImage, SamplingFilter, crop, resize, blend, padding_uniform, Rgba } from "@cf-wasm/photon";
 
 const app = new Hono();
 
@@ -9,27 +9,40 @@ app.get('/', (c) => {
 		<br>
 		The :textureId is from textures.minecraft.net.
 		<br>
+		<br>
 		Endpoints:
 		<br>
-		/cape/:textureId(?back)
+		/cape/:textureId/:width(?back)
 		<br>
-		/head/:textureId(?nooverlay)
+		Default Width: 80
+		<br>
+		<img src="/cape/953cac8b779fe41383e675ee2b86071a71658f2180f56fbce8aa315ea70e2ed6">
 		<br>
 		<br>
-		On error, it returns a Error 404 status code with JSON data {"error":true}
+		/face/:textureId/:size?(?nolayers)
+		<br>
+		Default Size: 64
+		<br>
+		<img src="/face/663904192ee4ca0df05dce2b2af677b3290db7311648b125fac140f616641dfa">
 		<br>
 		<br>
-		<a href="https://github.com/bribes/cf-minecraft-avatar-api">Github repo</a>
+		On error, it returns a Error 404 status code with JSON data:
+		<code>{"error":true}</code>
+		<br>
+		<br>
+		<a href="https://github.com/bribes/cf-minecraft-avatar-api">Github Repository</a>
 		<br>
 		Powered by Cloudflare Workers, Hono, and Photon.
 		`)
 })
 
-app.get('/cape/:textureId', async (c) => {
+app.get('/cape/:textureId/:width?', async (c) => {
 	try {
-		const textureId = c.req.param('textureId');
+		var textureId = c.req.param('textureId');
+		if (textureId.endsWith('.png')) textureId = textureId.split('.')[0];
 		const back = typeof c.req.query('back') == 'string';
 		const imageUrl = "http://textures.minecraft.net/texture/" + encodeURIComponent(textureId);
+		var width = c.req.param('width') ?? 80;
 
 		const inputBytes = await fetch(imageUrl)
 			.then((res) => res.arrayBuffer())
@@ -57,8 +70,8 @@ app.get('/cape/:textureId', async (c) => {
 
 		var outputImage = resize(
 			croppedImage,
-			80,
-			128,
+			width,
+			width*1.6,
 			SamplingFilter.Nearest
 		)
 
@@ -76,11 +89,13 @@ app.get('/cape/:textureId', async (c) => {
 	}
 })
 
-app.get('/head/:textureId', async (c) => {
+app.get('/face/:textureId/:size?', async (c) => {
 	try {
-		const textureId = c.req.param('textureId');
-		const noOverlay = typeof c.req.query('nooverlay') == 'string';
+		var textureId = c.req.param('textureId');
+		if (textureId.endsWith('.png')) textureId = textureId.split('.')[0];
+		const noLayers = typeof c.req.query('nolayers') == 'string';
 		const imageUrl = "http://textures.minecraft.net/texture/" + encodeURIComponent(textureId);
+		var size = c.req.param('size') ?? 64;
 
 		const inputBytes = await fetch(imageUrl)
 			.then((res) => res.arrayBuffer())
@@ -96,14 +111,16 @@ app.get('/head/:textureId', async (c) => {
 			16
 		);
 
-		var outputImage1 = resize(
-			croppedImage1,
-			64,
-			64,
-			SamplingFilter.Nearest
-		)
+		if (!noLayers) {
+			var outputImage1 = resize(
+				croppedImage1,
+				size-(size/9),
+				size-(size/9),
+				SamplingFilter.Nearest
+			)
 
-		if (!noOverlay) {
+			var outputImage1 = padding_uniform(outputImage1, (size/9)/2, new Rgba(0, 0, 0, 1))
+
 			var croppedImage2 = crop(
 				inputImage,
 				40,
@@ -114,8 +131,8 @@ app.get('/head/:textureId', async (c) => {
 
 			var outputImage2 = resize(
 				croppedImage2,
-				64,
-				64,
+				size,
+				size,
 				SamplingFilter.Nearest
 			)
 
@@ -123,6 +140,13 @@ app.get('/head/:textureId', async (c) => {
 				outputImage1,
 				outputImage2,
 				'over'
+			)
+		} else {
+			var outputImage1 = resize(
+				croppedImage1,
+				size,
+				size,
+				SamplingFilter.Nearest
 			)
 		}
 
@@ -132,7 +156,7 @@ app.get('/head/:textureId', async (c) => {
 		croppedImage1.free();
 		outputImage1.free();
 
-		if (!noOverlay) {
+		if (!noLayers) {
 			croppedImage2.free();
 			outputImage2.free();
 		}
